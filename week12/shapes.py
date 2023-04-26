dafd358fee6afb76b6a5d97bd167cfede9b41a5c
TDM\avigar <rafael.avigad@nmtafe.wa.edu.au>
2023-04-26 14:03:31 +0800

    solution to problem1

--- week12/shapes.py
+++ week12/shapes.py
@@ -1,6 +1,7 @@
 from math import pi
 class Circle:
-    def __init__(self, radius):
+    def __init__(self, name, radius):
+        self.name = name
         self.radius = radius
 
 
@@ -8,18 +9,22 @@ class Circle:
         return pi * self.radius**2 # print(...)
 
 class Square:
-    def __init__(self, length):
+    def __init__(self, name, length):
+        self.name = name
         self.length = length
 
     def area(self):
         return self.length**2
 
 def return_the_larger_shape_by_area(shape1, shape2):
-    pass
+    if shape1.area() > shape2.area():
+        return shape1
+    else:
+        return shape2
 
-circle = Circle(5)
-square = Square(5)
+circle = Circle('circle', 5)
+square = Square('square', 5)
 print(return_the_larger_shape_by_area(circle, square)) #return circle
 
-circle = Circle(5)
+
 
