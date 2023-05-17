2264de62e6ca5a26689f6a94a8114e48aac498d3
rafrafavi <rafraf@gmail.com>
2023-05-17 15:50:22 +0800

    final exercise: modify test_dog_name_in_bark

--- week15/unit_testing_overview.py
+++ week15/unit_testing_overview.py
@@ -1,3 +1,4 @@
+import unittest
 class Dog:
     def __init__(self, name, breed):
         self.name = name
@@ -6,7 +7,22 @@ class Dog:
     def bark(self):
         return f"{self.name} says woof!"
 
-if __name__ == '__main__':
+class TestDog(unittest.TestCase):
+    def setUp(self) -> None:
+        self.name = "Fido"
+        self.breed = "Labrador"
+        self.dog = Dog(self.name, self.breed)
+    def test_dog_bark_includes_name_of_dog(self):
+        self.assertEquals(self.dog.bark(),
+                          f"{self.name} says woof!")
+
+    def test_dog_name_in_bark(self):
+       name = "Fido"
+       fido = Dog(name, "Labrador")
+       result = fido.bark()
+       self.assertIn(name, result)
+
+if __name__ == '__not_main__':
     fido = Dog("Fido", "Labrador") # Fido says woof
     print(fido.bark())
     assert "Fido says woof!     " == fido.bark()
