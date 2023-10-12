22c32d41ba50856b26b01e44379d4d2385dc246f
Rafael Avigad <avigar@tdm.local>
2023-10-12 14:37:13 +0800

    lesson 12 demo

---
+++ week12/intro_doctsrings.py
@@ -0,0 +1,24 @@
+# Hello # comment
+"""Hello 
+World
+
+def hello():
+   something
+""" # multiline string
+"Hello\nWorld" #representing multiple lines on a single line
+# These are exactly the same
+"""Hello
+World"""
+class Cat:
+    '''This is a cute a cat (class docstring)'''
+
+def meow():
+    '''This makes a cat meow (function docstring)'''
+
+
+print(help(meow))
+
+#hover over for help
+meow()
+
+cat = Cat()
