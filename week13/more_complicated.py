3479dac3a4636fb66d7f8f34abcaf808b9cfc0ee
Rafael Avigad <avigar@tdm.local>
2023-10-19 15:03:19 +0800

    end of lesson

---
+++ week13/more_complicated.py
@@ -0,0 +1,15 @@
+def my_func():
+    print(hello)
+
+
+my_func()
+
+# Traceback (most recent call last):
+#   File "...source\repos\civ-ipriot-in-class-demos\week13\more_complicated.py
+#   ", line 4, in <module>
+#     my_func()
+#   File "...source\repos\civ-ipriot-in-class-demos\week13\more_complicated.py
+#   ", line 2, in my_func
+#     print(hello)
+#           ^^^^^
+# NameError: name 'hello' is not defined. Did you mean: 'help'?
