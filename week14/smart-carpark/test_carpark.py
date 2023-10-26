2a07efe25edf85412966d70bc9d41ba94207d355
Rafael Avigad <avigar@tdm.local>
2023-10-26 14:54:06 +0800

    initial test case

--- week14/smart-carpark/test_carpark.py
+++ week14/smart-carpark/test_carpark.py
@@ -4,3 +4,16 @@ from car import Car
 from car_park import CarPark
 from display import Display
 from sensor import Sensor
+
+class TestCarPark(unittest.TestCase):
+    def test_carpark_is_instantiated(self):
+        self.assertIsInstance(CarPark(), CarPark)
+
+    def test_car_is_instantiated(self):
+        self.assertIsInstance(Car(), Car)
+
+    def test_display_is_instantiated(self):
+        self.assertIsInstance(Display(), Display)
+
+    def test_sensor_is_instantiated(self):
+        self.assertIsInstance(Sensor(), Sensor)
