7db01e198e3f368f3fc76a5bd5bbec8f719548b8
Rafael Avigad <avigar@tdm.local>
2023-10-26 14:59:47 +0800

    initial car class

--- week14/smart-carpark/car.py
+++ week14/smart-carpark/car.py
@@ -1,2 +1,7 @@
 class Car:
-    ...
+    def __init__(self, plate = None):
+        if plate:
+            self.plate = plate
+        else:
+            print("Crazy illegal driver")
+            self.plate = plate
