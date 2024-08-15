f67b201aae965265a9fc1ee70392815cfce93ae8
Rafael A. <30427560+rafrafavi@users.noreply.github.com>
2024-08-15 16:32:46 +0800

    start of s2 repo!

---
+++ week5/intro_dictionaries.py
@@ -0,0 +1,20 @@
+# Problem
+# I want to look up results based on student numbers
+
+# Solution using a list
+results =    [['2001234', 'CO'],
+                       ['2001235', 'CO'],
+                       ['2001236', 'CO'],
+                       ['2004321', 'NYC']]
+
+results = ['2001234',
+           'CO',
+           '2001235',
+           'CO',
+           '2001236',
+           'CO',
+           '2004321',
+           'NYC']
+def get_grade(student_number, result_list):
+    result = "??"
+    return result
