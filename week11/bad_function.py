dd44d47513e1a0a75b194480cbadecc932fcadb5
unknown <a@b.com>
2025-04-29 18:48:59 +0800

    bad bad function

---
+++ week11/bad_function.py
@@ -0,0 +1,12 @@
+unit = 'c'
+temperature = 38.8
+
+def convert_temp():
+    global converted_temp
+    if unit.lower() == 'f':
+        converted_temp = (temperature - 32) * 5 / 9
+    else:
+        converted_temp = temperature * 9 / 5 + 32
+
+convert_temp()
+print(converted_temp)
