2ad8e370cfcd822ccc36d6685d25f22d2538792f
Para O'Kelly <para.okelly@nmtafe.wa.edu.au>
2026-05-20 19:56:36 +0800

    Swap annotation order! (sorry)

--- week15/exercise/loadable.py
+++ week15/exercise/loadable.py
@@ -2,8 +2,8 @@ import json
 from abc import ABC, abstractmethod
 
 class Loadable(ABC):
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def load(self, object_dictionary):
         # This should return an instance of whatever is inheriting Loadable
         raise NotImplementedError
